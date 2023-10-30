d5c8d30f8f17158bb3bc3be1af8959734ead2de4
Cody Logan <cody@lokken.dev>
2023-10-30 13:46:27 -0700

    Additional tests for batch file parsing

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -15,11 +15,16 @@
 # You should have received a copy of the GNU General Public License
 # along with Wikiget. If not, see <https://www.gnu.org/licenses/>.
 
+import io
+import logging
+from pathlib import Path
+from typing import Dict
+
 import pytest
 
 from wikiget.exceptions import ParseError
 from wikiget.file import File
-from wikiget.parse import get_dest
+from wikiget.parse import get_dest, read_batch_file
 from wikiget.wikiget import construct_parser
 
 
@@ -71,3 +76,62 @@ class TestGetDest:
         )
         _ = get_dest(args.FILE, args)
         assert "Target is a URL; ignoring site specified with --site" in caplog.text
+
+
+class TestReadBatchFile:
+    @pytest.fixture()
+    def dl_list(self, tmp_path: Path) -> Dict[int, str]:
+        tmp_file = tmp_path / "batch.txt"
+        tmp_file.write_text("File:Foo.jpg\nFile:Bar.jpg\nFile:Baz.jpg\n")
+        return read_batch_file(str(tmp_file))
+
+    def test_batch_file_log(self, caplog: pytest.LogCaptureFixture) -> None:
+        caplog.set_level(logging.INFO)
+        _ = read_batch_file("batch.txt")
+        assert "Using file 'batch.txt' for batch download" in caplog.text
+
+    def test_batch_file_length(self, dl_list: Dict[int, str]) -> None:
+        assert len(dl_list) == 3
+
+    def test_batch_file_contents(self, dl_list: Dict[int, str]) -> None:
+        expected_list = {1: "File:Foo.jpg", 2: "File:Bar.jpg", 3: "File:Baz.jpg"}
+        assert dl_list == expected_list
+
+    @pytest.fixture()
+    def dl_list_stdin(self, monkeypatch: pytest.MonkeyPatch) -> Dict[int, str]:
+        monkeypatch.setattr(
+            "sys.stdin", io.StringIO("File:Foo.jpg\nFile:Bar.jpg\nFile:Baz.jpg\n")
+        )
+        return read_batch_file("-")
+
+    def test_batch_stdin_log(
+        self, caplog: pytest.LogCaptureFixture, monkeypatch: pytest.MonkeyPatch
+    ) -> None:
+        caplog.set_level(logging.INFO)
+        monkeypatch.setattr("sys.stdin", io.StringIO("File:Foo.jpg\n"))
+        _ = read_batch_file("-")
+        assert "Using stdin for batch download" in caplog.text
+
+    def test_batch_stdin_length(self, dl_list_stdin: Dict[int, str]) -> None:
+        assert len(dl_list_stdin) == 3
+
+    def test_batch_stdin_contents(self, dl_list_stdin: Dict[int, str]) -> None:
+        expected_list = {1: "File:Foo.jpg", 2: "File:Bar.jpg", 3: "File:Baz.jpg"}
+        assert dl_list_stdin == expected_list
+
+    @pytest.fixture()
+    def dl_list_with_comment(self, tmp_path: Path) -> Dict[int, str]:
+        tmp_file = tmp_path / "batch.txt"
+        tmp_file.write_text("File:Foo.jpg\n\n#File:Bar.jpg\nFile:Baz.jpg\n")
+        return read_batch_file(str(tmp_file))
+
+    def test_batch_file_with_comment_length(
+        self, dl_list_with_comment: Dict[int, str]
+    ) -> None:
+        assert len(dl_list_with_comment) == 2
+
+    def test_batch_file_with_comment_contents(
+        self, dl_list_with_comment: Dict[int, str]
+    ) -> None:
+        expected_list = {1: "File:Foo.jpg", 4: "File:Baz.jpg"}
+        assert dl_list_with_comment == expected_list
